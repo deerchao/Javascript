8e782d696826f16fce2c8c59382fc9b2b90d1500
Chao Lu <deerchao@gmail.com>
2012-03-21 01:13:12 +0800

    added TagEditor

---
+++ Development/Controllers/TagEditorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Javascript.Controllers
+{
+    public class TagEditorController : Controller
+    {
+        private readonly string[] _allTags = new[]
+                                                 {
+                                                     "Ada",
+                                                     "Basic",
+                                                     "C",
+                                                     "C++",
+                                                     "C#",
+                                                     "D",
+                                                     "Delphi",
+                                                     "Effiel",
+                                                     "Effiel.Net",
+                                                     "F#",
+                                                     "Go",
+                                                     "Haskell",
+                                                     "Java",
+                                                     "Javascript",
+                                                     "Lua",
+                                                     "Lisp",
+                                                     "Python",
+                                                     "PHP",
+                                                     "Pascal",
+                                                     "Ruby",
+                                                     "Smalltalk",
+                                                     "Scala",
+                                                     "Visual Basic",
+                                                     "Visual FoxPro",
+                                                 };
+
+        [HttpPost]
+        public ActionResult Tags(string start)
+        {
+            var tags = _allTags.Where(x => x.StartsWith(start, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+            return Json(tags);
+        }
+    }
+}
